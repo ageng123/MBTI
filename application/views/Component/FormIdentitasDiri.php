1c0ed9d3c0d3c92a1494a6925c19ea20d6321a9a
ageng123 <ageng133@gmail.com>
2020-08-21 16:02:13 +0700

    Update Api.php, FormIbuKandung.php, and 4 more files...

--- application/views/Component/FormIdentitasDiri.php
+++ application/views/Component/FormIdentitasDiri.php
@@ -1,7 +1,6 @@
 <legend>1. Identitas Diri</legend>
 <div class="col-md-12">
 	<div class="form-group">
-<<<<<<< HEAD
 		<label>Foto</label>
 		<input type="file" name="identity['foto']" class="form-control-file" required=""/>
 	</div>
@@ -10,36 +9,23 @@
 	<div class="form-group">
 		<label>Nama Lengkap</label>
 		<input type="text" name="identity['nama']" class="form-control" required=""/>
-=======
-		<label>Nama Lengkap</label>
-		<input type="text" name="nama" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Nama Alias</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['alias']" class="form-control" required=""/>
-=======
-		<input type="text" name="alias" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-3">
 	<div class="form-group">
 		<label>Tempat Lahir</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['tempat']" class="form-control" required=""/>
-=======
-		<input type="text" name="tempatlahir" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-9">
 	<div class="form-group">
 		<label>Tanggal Lahir</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['tanggal']" class="form-control datepicker" required=""/>
 	</div>
 </div>
@@ -50,129 +36,78 @@
 			<option value="laki-laki">Laki-Laki</option>
 			<option value="perempuan">Perempuan</option>
 		</select>
-=======
-		<input type="text" name="tanggallahir" class="form-control datepicker" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Suku Bangsa/Ras</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['suku']" class="form-control" required=""/>
-=======
-		<input type="text" name="suku" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Pekerjaan / Jabatan </label>
-<<<<<<< HEAD
 		<input type="text" name="identity['pekerjaan']" class="form-control" required=""/>
-=======
-		<input type="text" name="pekerjaan" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Pangkat / Golongan / NRP / NIP</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['pangkat']" class="form-control" required=""/>
-=======
-		<input type="text" name="pangkat" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Kesatuan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['kesatuan']" class="form-control" required=""/>
-=======
-		<input type="text" name="kesatuan" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Alamat dan Nomor Telepon Kantor</label>
-<<<<<<< HEAD
 		<textarea name="identity['alamatkantor']" id="" cols="30" rows="10" class="form-control"></textarea>
-=======
-		<textarea name="alamatkantor" id="" cols="30" rows="10" class="form-control"></textarea>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Alamat dan Nomor Telepon Rumah</label>
-<<<<<<< HEAD
 		<textarea name="identity['alamatrumah']" id="" cols="30" rows="10" class="form-control"></textarea>
-=======
-		<textarea name="alamatrumah" id="" cols="30" rows="10" class="form-control"></textarea>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Kewarganegaraan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['kewarganegaraan']" class="form-control" required=""/>
-=======
-		<input type="text" name="kewarganegaraan" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Cara Memperoleh Kewarganegaraan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['caramemperoleh']" class="form-control" required=""/>
-=======
-		<input type="text" name="carakewarganegaraan" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Agama yang Dianut Saat Ini</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['now_agama']" class="form-control" required=""/>
-=======
-		<input type="text" name="agama" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Agama yang dianut sebelumnya</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['old_agama']" class="form-control" required=""/>
-=======
-		<input type="text" name="oldagama" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Aliran Kepercayaan yang Diikuti</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['aliran']" class="form-control" required=""/>
-=======
-		<input type="text" name="alira" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Kawin / Belom Kawin</label>
-<<<<<<< HEAD
 		<select name="identity['status']" id="" class="form-control">
-=======
-		<select name="statuspernikahan" id="" class="form-control">
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 			<option value="	">Pilih Status</option>
 			<option value="Kawin"> Kawin </option>
 			<option value="Belum"> Belum Kawin </option>
@@ -182,90 +117,54 @@
 <div class="col-md-3">
 	<div class="form-group">
 		<label>Tempat Pernikahan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['tempat_nikah']" class="form-control" required=""/>
-=======
-		<input type="text" name="tempatlahir" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-9">
 	<div class="form-group">
 		<label>Tanggal Pernikahan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['tanggal_nikah']" class="form-control datepicker" required=""/>
-=======
-		<input type="text" name="tanggallahir" class="form-control datepicker" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Perkawinan yang keberapa saat ini</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['jumlah_nikah']" class="form-control" required=""/>
-=======
-		<input type="text" name="jumlahperkawinan" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Jumlah Anak</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['jumlah_anak']" class="form-control" required=""/>
-=======
-		<input type="text" name="jumlahanak" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Alamat Tetap sesuai KTP dan Nomor Telepon</label>
-<<<<<<< HEAD
 		<textarea name="identity['alamat_ktp']" class="form-control" id="" cols="30" rows="10"></textarea>
-=======
-		<textarea name="alamatktp" class="form-control" id="" cols="30" rows="10"></textarea>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Nomor Identitas (SIM/KTP/identitas pengenal lain)</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['nomor_identitas']" class="form-control" required=""/>
-=======
-		<input type="text" name="nomoridentitas" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Hobi/Kegemaran</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['hobi']" class="form-control" required=""/>
-=======
-		<input type="text" name="hobi" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Kebiasaan yang sukar dihilangkan</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['kebiasaan']" class="form-control" required=""/>
-=======
-		<input type="text" name="kebiasaansukar" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
 <div class="col-md-12">
 	<div class="form-group">
 		<label>Kemampuan Berbahasa (Daerah/Asing/Nasional)</label>
-<<<<<<< HEAD
 		<input type="text" name="identity['bahasa']" class="form-control" required=""/>
-=======
-		<input type="text" name="bahasa" class="form-control" required=""/>
->>>>>>> a0a695a3b6a41a16eb3f089a47e2910c7f01a38c
 	</div>
 </div>
